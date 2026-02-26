c02286d524b7786fac31874acb186228504504d6
John Patrick Ombao <www.ombaojohnpatrick@gmail.com>
2026-02-26 23:35:31 +0800

    inventory sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid,  PackageSearch} from 'lucide-react';
+import { BookOpen, Folder, LayoutGrid, PackageSearch, Store } from 'lucide-react';
 import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
@@ -25,6 +25,11 @@ const mainNavItems: NavItem[] = [
         title: 'Products',
         href: '/products',
         icon: PackageSearch,
+    },
+    {
+        title: 'Inventory',
+        href: '/inventory',
+        icon: Store,
     },
 ];
 
